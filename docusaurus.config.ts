f0272c8df77b343c9d51a904d4cfa673e3d81eae
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2025-12-06 23:28:07 +0500

    Update baseUrl and url for Vercel deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://BilqeesShahid.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/myAIbook/',
+url: 'https://my-ai-book-ten.vercel.app', // your Vercel URL
+// Set the /<baseUrl>/ pathname under which your site is served
+baseUrl: '/', // root path for Vercel
+
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
