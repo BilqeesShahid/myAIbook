ff139d2c99da15d353a7a9655c2bcebd843dcd6b
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2025-12-06 23:07:28 +0500

    Fix Docusaurus config for deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,8 @@ const config: Config = {
   organizationName: 'BilqeesShahid', // Usually your GitHub org/user name.
   projectName: 'myAIbook', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
